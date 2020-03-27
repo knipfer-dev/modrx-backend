44772b270166bbb34ee49c200439a725f267ed84
alexknipfer <alexanderknipfer@gmail.com>
2020-03-27 19:41:13 -0400

    Initial commit and add healthcheck endpoint

---
+++ Modrx/Controllers/HealthcheckController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Modrx.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HealthcheckController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(new { timestamp = DateTime.Now.ToString("o") });
+        }
+    }
+}
